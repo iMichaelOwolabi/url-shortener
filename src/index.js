aa1243c0fcde70360317b92f3d16e8db0424ad9a
Michael Owolabi <michael.i.owolabi@gmail.com>
2021-03-22 21:21:34 +0100

    implement the frontend using template engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const expressHandlebars = require('express-handlebars');
 const { config } = require('./utils');
 const shortenerRouter = require('./routes/shortUrl');
-const redirectROuter = require('./routes/redirectUrl');
+const redirectRouter = require('./routes/redirectUrl');
+const homeRouter = require('./routes/index');
 
 const port = config.serverPort || 7000;
+const publicDircetoryPath = path.join(__dirname, '../public');
 
 const router = express;
 
@@ -11,11 +16,18 @@ const app = express();
 
 app.use(express.json({}));
 
+app.engine('handlebars', expressHandlebars());
+app.set('view engine', 'handlebars');
+
+// Setup static directory to serve
+app.use(express.static(publicDircetoryPath));
+
 app.listen(port, () => {
   console.log(`server listening on port ${port} 🚀`);
 });
 
 app.use('/v1/shorten', shortenerRouter);
-app.use('/v1/redirect', redirectROuter);
+app.use('/r', redirectRouter);
+app.use('', homeRouter);
 
-module.exports = { router };
+module.exports = { router, app };
